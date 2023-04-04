c1e91b36413e292e1f3a56e2fce4975df142e077
SvetlanaPanteleeva <123400566+SvetlanaPanteleeva@users.noreply.github.com>
2023-04-04 18:20:28 +0300

    Добавьте файлы проекта.

---
+++ Wpf19/Models/Length.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf19.Models
+{
+    static class Length
+    {
+       public static double Add(double r) => 2 * r * Math.PI;
+    }
+}
